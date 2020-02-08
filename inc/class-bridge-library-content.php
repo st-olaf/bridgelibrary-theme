4aa762c9ce916c3b273dc34377ec9040f5a5d56f
SarahZum <johnston.sarah@gmail.com>
2020-02-08 08:52:15 -0600

    Updated login link to redirect to Google Auth automatically

--- inc/class-bridge-library-content.php
+++ inc/class-bridge-library-content.php
@@ -51,7 +51,7 @@ class Bridge_Library_Content {
 	 */
 	public function page_content() {
 		if ( is_front_page() && ! is_user_logged_in() ) {
-			echo '<p>Please <a href="https://accounts.google.com/o/oauth2/auth?response_type=code&amp;redirect_uri=https%3A%2F%2Fmy.bridgelibraries.org%2Fwp-login.php&amp;client_id=364900329230-ol63f99o4mtijsu23cfeoq2ru0r77v3l.apps.googleusercontent.com&amp;scope=openid+email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&amp;access_type=online&amp;approval_prompt=auto&amp;state=77dba580fe%257C%252F"><img src="/wp-content/plugins/googleappslogin-premium/img/btn_google_signin_dark_normal_web.png" alt="Sign In with Google"></a> using your college Gmail account.</p>';
+            echo '<p>Please <a href="/wp-login.php?gaautologin=true&redirect_to=https://my.bridgelibraries.org/"><img src="https://my.bridgelibraries.org/wp-content/plugins/googleappslogin-premium/img/btn_google_signin_dark_normal_web.png"></a> using your college Gmail account.</p>';
 		} else {
 			get_template_part( 'template-parts/react', 'none' );
 		}
