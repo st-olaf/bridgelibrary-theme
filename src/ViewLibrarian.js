9f5d02d74804b07065db37b55e857afbe778dd7e
SarahZum <johnston.sarah@gmail.com>
2020-02-14 10:06:04 -0600

    Made adjustments to display image, email link, and profile link on librarian card

--- src/ViewLibrarian.js
+++ src/ViewLibrarian.js
@@ -5,63 +5,19 @@ class ViewLibrarian extends React.Component {
     // TODO: add phoot.
 
     render() {
-        let librarianData = this.props.parentState.currentObject.librarianData;
-        let meta = [];
-
-        if (librarianData) {
-            meta = [
-                {
-                    type: "Academic Departments",
-                    value: librarianData.academicDepartment.reduce(
-                        (departmentString, department) => {
-                            // Handle first value.
-                            if ("object" === typeof departmentString) {
-                                departmentString = departmentString.name;
-                            }
-
-                            return departmentString + ", " + department.name;
-                        }
-                    )
-                }
-            ];
-
-            if (
-                librarianData.librarianUserId &&
-                librarianData.librarianUserId.userData
-            ) {
-                let userData = librarianData.librarianUserId.userData;
-
-                meta.push(
-                    {
-                        type: "Email Address",
-                        value: userData.librarian.emailAddress
-                    },
-                    {
-                        type: "Phone Number",
-                        value: userData.librarian.phoneNumber
-                    },
-                    {
-                        type: "Office Location",
-                        value: userData.librarian.officeLocation
-                    },
-                    {
-                        type: "Website",
-                        value: userData.librarian.website
-                    },
-                    {
-                        type: "Photo",
-                        value: userData.librarian.picture
-                    }
-                );
-            }
-        }
+        console.log(this.props);
 
         return (
             <div className="entry-content clear">
-                <ul>
-                    {meta.length > 0 &&
-                        meta.map((meta, i) => <Meta key={i} meta={meta} />)}
-                </ul>
+                {null !== this.props.parentState.currentObject.librarianData.librarianUserId.userData.librarian.picture ?
+                    (<img src={this.props.parentState.currentObject.librarianData.librarianUserId.userData.librarian.picture.sourceUrl} />)
+                    : (<img src={this.props.parentState.currentObject.librarianData.librarianUserId.userData.pictureUrl} />)}
+                <p><a href={this.props.parentState.currentObject.librarianData.librarianUserId.userData.librarian.website}>See Full Profile</a></p>
+                <p>{this.props.parentState.currentObject.librarianData.librarianUserId.userData.librarian.officeLocation}</p>
+                <p>{this.props.parentState.currentObject.librarianData.librarianUserId.userData.librarian.phoneNumber}</p>
+                <p><a href={"mailto:" + this.props.parentState.currentObject.librarianData.librarianUserId.userData.librarian.emailAddress}>{this.props.parentState.currentObject.librarianData.librarianUserId.userData.librarian.emailAddress}</a>
+                </p>
+
             </div>
         );
     }
