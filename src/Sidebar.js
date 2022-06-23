b5082e1faa6fae7eece71d8e7319cf0e2dd7bfcb
Andrew Minion <andrew.minion@luminfire.com>
2022-06-23 17:26:10 -0500

    disable user interest feeds display
    Closes https://git.luminfire.net/luminfire/clients/stolaf-bridge/bridge-library-plugin/-/issues/28

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -63,7 +63,7 @@ class Sidebar extends React.Component {
                                 ) : (
                                     <CirculationWidget {...this.props} />
                                 )}
-                                <UserInterestFeedsWidget {...this.props} />
+                                {/* <UserInterestFeedsWidget {...this.props} /> */}
                             </ul>
                         </div>
                     </aside>
