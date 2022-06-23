adbadc57dad1d2419e156b0985b666a2773600ab
Andrew Minion <andrew.minion@luminfire.com>
2022-06-23 17:25:29 -0500

    update local TLD and path

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ npm start &
 # Run file watcher to automatically compile sass to css
 sass --watch assets/sass/style.scss:assets/css/style.css --style=compressed &
 
-browser-sync start --files="assets/css/style.css,**/*.js,*.php,**/*.php" --open=external --proxy=https://bridge-st-olaf-carleton-library.private --host=$(hostname) &
+browser-sync start --files="assets/css/style.css,**/*.js,*.php,**/*.php" --open=external --proxy=https://bridge-st-olaf-carleton-library.test --host=$(hostname) &
