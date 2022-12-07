5cecc48db428de8ad2060c6f0d260bdf5c61e3e3
Andrew Minion <andrew.minion@luminfire.com>
2022-12-07 14:38:25 -0600

    remove hidden data

--- template-parts/card-course.php
+++ template-parts/card-course.php
@@ -22,14 +22,5 @@
 	} elseif ( ! empty( $primo_image_info ) ) {
 		echo '<a href="' . esc_url( get_the_permalink() ) . '"><img class="primo callback" src="' . esc_url( $primo_image_info ) . '" alt="Image from Google Books for ' . esc_attr( get_the_title() ) . '" /></a>';
 	}
-
-	$departments = get_the_terms( get_the_ID(), 'academic_department' );
-	$terms       = get_the_terms( get_the_ID(), 'course_term' );
-
 	?>
-	<ul class="meta">
-		<li class="term"><?php echo wp_kses_post( _n( 'Term: ', 'Terms: ', count( $terms ), 'bridge-library' ) . ' ' . implode( ', ', wp_list_pluck( $terms, 'name' ) ) ); ?></li>
-		<li class="department"><?php echo wp_kses_post( _n( 'Department: ', 'Departments: ', count( $departments ), 'bridge-library' ) . ' ' . implode( ', ', wp_list_pluck( $departments, 'name' ) ) ); ?></li>
-	</ul>
-	<p class="description"><?php the_content(); ?></p>
 </div>
